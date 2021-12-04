5742389afca94579e2f4669f2057b8730985205a
Eldar Khasmamedov <ekhasm@gmail.com>
2021-12-04 18:43:33 -0500

    First commit

---
+++ ReceiptTracker/App/ReceiptTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReceiptTrackerApp.swift
+//  ReceiptTracker
+//
+//  Created by Eldar Khasmamedov on 2021-12-04.
+//
+
+import SwiftUI
+
+@main
+struct ReceiptTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
---
+++ ReceiptTracker/MainView/MainView.swift
@@ -0,0 +1,21 @@
+//
+//  MainView.swift
+//  ReceiptTracker
+//
+//  Created by Eldar Khasmamedov on 2021-12-04.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("Main View")
+            .padding()
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
